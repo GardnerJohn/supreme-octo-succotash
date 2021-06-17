8238eb2985430d93180cb2df21704910c0eb907a
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-06-16 19:09:41 -0700

    hamburgerIcon added with styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,34 @@
-import React from "react";
-import { Nav, NavbarContainer, NavLogo, NavIcon } from './Navbar.elements';
-
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  HamburgerIcon,
+} from "./Navbar.elements";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">
-            <NavIcon />
-            ULTRA
-          </NavLogo>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <HamburgerIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </HamburgerIcon>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
