d50f71fc80e6c7f7d1099df7c63229bd762dce7a
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-07-08 10:13:53 -0700

    Home, InfoSection added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import GlobalStyle from "./globalStyles";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components";
-
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Navbar />
-      <Switch></Switch>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </Router>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Navbar } from "./Navbar/Navbar.js";
+export { default as InfoSection } from "./InfoSection/InfoSection.js";
