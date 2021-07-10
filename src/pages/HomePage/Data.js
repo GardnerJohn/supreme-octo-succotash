fdaf8bcdeb063667716e374de4e11a41b929d743
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-07-10 08:03:06 -0700

    Home page structure with example pics added

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,7 +1,6 @@
 export const homeObjOne = {
   lightBg: false,
   primary: true,
-  lightBg: false,
   imgStart: "",
   lightTopLine: true,
   lightTextDesc: true,
@@ -11,7 +10,58 @@ export const homeObjOne = {
   headline: "Lead Generation Specialist for Online Businesses",
   lightText: true,
   topLine: "Margeting Agency",
-  img: require("../../images/programmer.png"),
+  img: require("../../images/programmer.png").default,
   alt: "Image",
   start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.",
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightText: false,
+  topLine: "Margeting Agency",
+  img: require("../../images/programmer.png").default,
+  alt: "Image",
+  start: "true",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.",
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightText: true,
+  topLine: "Margeting Agency",
+  img: require("../../images/programmer.png").default,
+  alt: "Image",
+  start: "",
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: "start",
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.",
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightText: true,
+  topLine: "Margeting Agency",
+  img: require("../../images/programmer.png").default,
+  alt: "Image",
+  start: "start",
 };
