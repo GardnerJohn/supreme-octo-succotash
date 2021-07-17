33c8c7deb61ceb680eacfed6fa94f2bd750c6749
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-07-16 17:12:18 -0700

    Removed extranious code

--- src/pages/SignUp/Data.js
+++ src/pages/SignUp/Data.js
@@ -13,21 +13,6 @@ export const homeObjOne = {
   alt: "Credit Card",
 };
 
-export const homeObjTwo = {
-  lightBg: false,
-  lightText: true,
-  lightTopLine: true,
-  lightTextDesc: true,
-  topLine: "100% Secure",
-  headline: "Stay protected 24/7 anywhere anytime",
-  description:
-    "We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime",
-  buttonLabel: "Learn More",
-  imgStart: "",
-  img: require("../../images/programmer.png"),
-  alt: "Vault",
-};
-
 export const homeObjThree = {
   lightBg: true,
   lightText: false,
@@ -38,21 +23,6 @@ export const homeObjThree = {
   description:
     "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
   buttonLabel: "Start Now",
-  imgStart: "start",
-  img: require("../../images/programmer.png"),
-  alt: "Vault",
-};
-
-export const homeObjFour = {
-  lightBg: false,
-  lightText: true,
-  lightTopLine: true,
-  lightTextDesc: true,
-  topLine: "Data Analytics",
-  headline: "Every transaction is stored on our secure cloud database",
-  description:
-    "Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.",
-  buttonLabel: "Sign Up Now",
   imgStart: "start",
   img: require("../../images/programmer.png"),
   alt: "Vault",
